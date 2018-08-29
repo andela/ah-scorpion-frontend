314034591d24232fc4221d8e0867c5eca1379e0f
blac-siren <zac.husnein@gmail.com>
2018-08-29 09:55:45 +0300

    [Chore #159993328] Update test

--- src/routes.test.js
+++ src/routes.test.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 import { MemoryRouter } from 'react-router-dom';
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from "redux";
+import reducers from "./reducers";
+
+
+const store = createStore(reducers, {}, applyMiddleware());
 
 it('renders the routes without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
+      <Provider store={store}>
     <MemoryRouter>
       <Routes/>
     </MemoryRouter>
+      </Provider>
     , div);
   ReactDOM.unmountComponentAtNode(div);
 

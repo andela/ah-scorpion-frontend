6c09fd46baebc00e4da83f1d713a86a47831d6af
davenjeru <dave.njeru@andela.com>
2018-08-31 12:37:00 +0300

    [Feature #159993338] add login with facebook
    Signed-off-by: davenjeru <dave.njeru@andela.com>

--- src/containers/SocialLogin.js
+++ src/containers/SocialLogin.js
@@ -1,34 +1,39 @@
-// Here we have a "smart" component which is aware of Redux
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { GoogleLogin } from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
+import PropTypes from 'prop-types';
 import * as actions from '../actions';
 
-const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+const fbAppId = process.env.REACT_APP_FACEBOOK_APP_ID;
 
 
 class SocialLogin extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
+  constructor({
+    SOCIALAUTH, isAuthenticated, message, loading,
+  }) {
+    super({
+      SOCIALAUTH, isAuthenticated, message, loading,
+    });
+    this.state = {};
 
     this.googleResponse = this.googleResponse.bind(this);
+    this.facebookResponse = this.facebookResponse.bind(this);
     this.socialAuthResponse = this.socialAuthResponse.bind(this);
     this.onFailure = this.onFailure.bind(this);
-    this.logout = this.logout.bind(this);
   }
 
   onFailure(error) {
     console.log(error);
   }
 
-  logout() {
-    this.setState({ isAuthenticated: false, token: '', user: null });
-  }
-
   googleResponse(response) {
     this.socialAuthResponse(response.accessToken, 'google-oauth2');
+  }
+
+  facebookResponse(response) {
+    this.socialAuthResponse(response.accessToken, 'facebook');
   }
 
   socialAuthResponse(token, provider) {
@@ -37,20 +42,35 @@ class SocialLogin extends Component {
       access_token: token,
       provider,
     };
-    this.props.SOCIALAUTH(requestBody);
+    this.SOCIALAUTH(requestBody);
   }
 
   render() {
     return (
-      <GoogleLogin
-        clientId={clientId}
-        buttonText="Login"
-        onSuccess={this.googleResponse}
-        onFailure={this.onFailure}
-      />
+      <div className="text-center">
+        <FacebookLogin
+          appId={fbAppId}
+          autoLoad
+          fields="name,email,picture"
+          callback={this.facebookResponse}
+          cssClass="btn btn-primary"
+          icon="fa fa-facebook fa-2x"
+          textButton=""
+        />
+
+        <GoogleLogin
+          clientId={googleClientId}
+          buttonText="Login"
+          onSuccess={this.googleResponse}
+          onFailure={this.onFailure}
+        />
+      </div>
     );
   }
 }
+SocialLogin.propTypes = {
+  SOCIALAUTH: PropTypes.func.isRequired,
+};
 
 const mapStateToProps = ({ authState }) => ({
   isAuthenticated: authState.isAuthenticated,

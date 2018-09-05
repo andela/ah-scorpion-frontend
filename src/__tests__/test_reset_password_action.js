a8645c8840c0fe8687aa5ed835bdac6e9ff1aa2f
Ruth Njeri Waiganjo <waiganjo@Ruths-MacBook-Pro-2.local>
2018-09-05 15:50:46 +0300

    [ft #159993336] add tests for actions

---
+++ src/__tests__/test_reset_password_action.js
@@ -0,0 +1,43 @@
+import moxios from 'moxios';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import reset from '../actions/resetPassword';
+
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+const userEmail = { user: { email: 'ruthnwaiganjo@gmail.com' } }
+
+const ResetResponse = {
+    resetData: userEmail
+};
+
+describe('reset password action', () => {
+    beforeEach(() => moxios.install());
+    afterEach(() => moxios.uninstall());
+
+    describe('Create reset password Action', () => {
+        it('Should dispatch reset action', async done => {
+            moxios.stubRequest(
+                '../api/reset',
+                { response: userEmail },
+
+            );
+            const returnedAction = [
+                {
+                    type: 'RESET_REQUEST_START',
+                }
+            ];
+
+            const store = mockStore({});
+            await store.dispatch(
+                reset({
+                    ...ResetResponse.resetData
+                })
+            );
+            expect(store.getActions()).toEqual(returnedAction);
+            done();
+        });
+    });
+});
--- src/index.js
+++ src/index.js
@@ -2,25 +2,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-<<<<<<< HEAD
 import thunk from 'redux-thunk';
-=======
->>>>>>> change routes to app.js file
 import ReactDOM from 'react-dom';
 import Thunk from 'redux-thunk';
 import reducers from './reducers';
 import './index.css';
 import App from './App';
 
-<<<<<<< HEAD
-const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(thunk)));
-=======
 const store = createStore(
   reducers,
   {},
-  composeWithDevTools(applyMiddleware(Thunk))
+  composeWithDevTools(applyMiddleware(thunk)),
 );
->>>>>>> change routes to app.js file
 
 ReactDOM.render(
   <Provider store={store}>

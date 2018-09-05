28536c45bbfedd132f2d2bd10480cc6e51dd7657
lenny <lennykmutua@gmail.com>
2018-09-05 15:40:39 +0300

    [ch 159993335] Add tests for actions

--- src/__tests__/SignUp.test.js
+++ src/__tests__/SignUp.test.js
@@ -1,5 +1,18 @@
+import moxios from 'moxios';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import { signUpSuccess, signUpError } from '../actions/signUp';
 import signUpReducer from '../reducers/signUp';
 import * as types from '../actions/types';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const emailError = { email: ['Email already exists, please login or use a different email'] };
+const successMessage = {
+  Message: 'Please confirm your email address to complete the registration',
+};
+const regUrl = 'https://authors-haven-api.herokuapp.com/api/v1/users/signup/';
 
 describe('The SignUp reducer', () => {
   const initialState = {
@@ -35,5 +48,51 @@ describe('The SignUp reducer', () => {
       ...initialState,
       success: true,
     });
+  });
+});
+
+describe('The SignUp actions', () => {
+  beforeEach(() => moxios.install());
+  afterEach(() => moxios.uninstall());
+
+  it('should dispatch the signUpSuccess action', async (done) => {
+    moxios.stubRequest(regUrl, {
+      status: 201,
+      response: successMessage,
+    });
+
+    const returnedAction = [
+      {
+        type: types.SIGNUP_SUCCESS,
+      },
+    ];
+
+    const store = mockStore({});
+    await store.dispatch(signUpSuccess());
+    expect(store.getActions()).toEqual(returnedAction);
+    done();
+  });
+
+  it('should dispatch the signUpError action', async (done) => {
+    moxios.stubRequest(regUrl, {
+      status: 404,
+      response: emailError,
+    });
+
+    const returnedAction = [
+      {
+        type: types.SIGNUP_ERROR,
+        error: emailError,
+      },
+    ];
+
+    const store = mockStore({});
+    await store.dispatch(
+      signUpError({
+        email: emailError.email,
+      }),
+    );
+    expect(store.getActions()).toEqual(returnedAction);
+    done();
   });
 });

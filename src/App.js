6ad09ff36ff1542410e8f5e28f16835d014262d3
blac-siren <zac.husnein@gmail.com>
2018-09-05 18:01:51 +0300

    [Featiure #159993337] Change pure function"

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './containers/Login';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/login" exact component={Login} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" exact component={Login} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;

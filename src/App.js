9b4a9168802b7435d51ed59b6c10fb335f2cf1db
blac-siren <zac.husnein@gmail.com>
2018-09-06 10:14:28 +0300

    [Featiure #159993337] install babel-eslint ' '

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './containers/Login';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login" exact component={Login} />
-      </Switch>
-    </BrowserRouter>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" exact component={Login} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 export default App;

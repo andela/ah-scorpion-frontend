dc1891db94b57d8eac10a6f16260450104d4d6f9
lenny <lennykmutua@gmail.com>
2018-09-06 10:03:14 +0300

    [ch #159993335] conform to eslint guides

--- src/__tests__/SignUpForm.test.js
+++ src/__tests__/SignUpForm.test.js
@@ -22,6 +22,12 @@ describe('The Login Form', () => {
       .toEqual(4);
     expect(home.find('button').length)
       .toEqual(3);
+
+describe('The Login Form', () => {
+  it('Should render without crashing', () => {
+    const home = shallow(<SignUpForm />);
+    expect(home.find('input').length).toEqual(4);
+    expect(home.find('button').length).toEqual(3);
   });
 });
 
@@ -29,32 +35,26 @@ describe('The Login Container', () => {
   const store = createStore(reducers, {}, applyMiddleware());
   it('contains the nav bar component on initialization', () => {
     const wrapper = mount(<SignUp store={store} success={false} />);
-    expect(wrapper.find('NavBar').length)
-      .toEqual(1);
+    expect(wrapper.find('NavBar').length).toEqual(1);
   });
 
   it('contains the one footer component on initialization', () => {
     const wrapper = mount(<SignUp store={store} success={false} />);
-    expect(wrapper.find('Footer').length)
-      .toEqual(1);
+    expect(wrapper.find('Footer').length).toEqual(1);
   });
 
   it('contains the signup form component on initialization', () => {
     const wrapper = mount(<SignUp store={store} success={false} />);
-    expect(wrapper.find('SignUpForm').length)
-      .toEqual(1);
+    expect(wrapper.find('SignUpForm').length).toEqual(1);
   });
 
   it('does not contain the success message component on initialization', () => {
     const wrapper = mount(<SignUp store={store} success={false} />);
-    expect(wrapper.find('ConfirmEmail').length)
-      .toEqual(0);
+    expect(wrapper.find('ConfirmEmail').length).toEqual(0);
   });
-
 
   it('does not contain the error message component on initialization', () => {
     const wrapper = mount(<SignUp store={store} success={false} />);
-    expect(wrapper.find('SignupError').length)
-      .toEqual(0);
+    expect(wrapper.find('SignupError').length).toEqual(0);
   });
 });

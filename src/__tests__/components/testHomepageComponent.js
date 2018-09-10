50f63ba0c0b20fd890a47da0f15ab9f644e6a8ac
lenny <lennykmutua@gmail.com>
2018-09-10 11:26:24 +0300

    [ch 160311539] Fix merge conflicts

--- src/__tests__/components/testHomepageComponent.js
+++ src/__tests__/components/testHomepageComponent.js
@@ -13,34 +13,33 @@ import reducers from '../../reducers/index';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('The Home Page', () => {
-  it('Should render without crashing', () => {
-    const home = mount(<MemoryRouter><Homepage /></MemoryRouter>);
-    expect(home.find('div').length).toEqual(49);
+  const store = createStore(reducers, {}, applyMiddleware());
+  let wrapper;
+  beforeAll(() => {
+    wrapper = mount(<MemoryRouter><Homepage store={store} success={false} /></MemoryRouter>);
   });
 
-  const store = createStore(reducers, {}, applyMiddleware());
+  it('Should render without crashing', () => {
+    expect(wrapper.find('div').length).toEqual(49);
+  });
+
   it('contains the footer component on initialization', () => {
-    const wrapper = mount(<MemoryRouter><Homepage store={store} success={false} /></MemoryRouter>);
     expect(wrapper.find('HomeFooter').length).toEqual(1);
   });
 
   it('contains three images on initialization', () => {
-    const wrapper = mount(<MemoryRouter><Homepage store={store} success={false} /></MemoryRouter>);
     expect(wrapper.find('img').length).toEqual(3);
   });
 
   it('initializes the Benefits components', () => {
-    const wrapper = mount(<MemoryRouter><Homepage store={store} success={false} /></MemoryRouter>);
     expect(wrapper.find('RenderBenefits').length).toEqual(1);
   });
 
   it('initializes the Features components', () => {
-    const wrapper = mount(<MemoryRouter><Homepage store={store} success={false} /></MemoryRouter>);
     expect(wrapper.find('RenderFeatures').length).toEqual(1);
   });
 
   it('initializes the Testimonials components', () => {
-    const wrapper = mount(<MemoryRouter><Homepage store={store} success={false} /></MemoryRouter>);
     expect(wrapper.find('RenderTestimonials').length).toEqual(1);
   });
 });
--- src/components/DashnoardContainer.js
+++ src/components/DashnoardContainer.js
@@ -12,7 +12,9 @@ const DashboardContainer = () => (
             <p className="lead text-white">
                 Something short and leading about the collection
                 below—its contents, the creator, etc. Make it short
-                and sweet, but not too short so folks don't simply skip over it entirely.
+                and sweet, but not too short so folks don
+              {'\''}
+              t simply skip over it entirely.
             </p>
             <a href="#" className="btn btn-primary m-2">Read More</a>
           </div>

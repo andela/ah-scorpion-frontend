3578b85bba953990f569fb74263766680e9fa487
davenjeru <dave.njeru@andela.com>
2018-09-10 12:50:18 +0300

    [Chore #160311539] remove test reducer
    Signed-off-by: davenjeru <dave.njeru@andela.com>

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,7 +2,6 @@ export const BEGIN_LOGIN = 'BEGIN_LOGIN';
 export const FAIL_LOGIN = 'FAIL_LOGIN';
 export const SUCCESS_LOGIN = 'SUCCESS_LOGIN';
 export const USER_LOGGED_IN = 'USER_LOGGED_IN';
-export const TEST_REDUX = 'TEST_REDUX';
 export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
 export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
 export const SIGNUP_ERROR = 'SIGNUP_ERROR';

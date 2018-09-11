98fa29d71cbceee65997f03499200ced7ce541dd
blac-siren <zac.husnein@gmail.com>
2018-09-11 12:57:15 +0300

    [Fix] Fix Typo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ResetForm from './containers/ResetPassword';
 import ConfirmPasswordForm from './containers/ConfirmPasswordForm';
 import SignUp from './containers/SignUp';
 import Dashboard from './containers/Dashboard';
-import { PrivateRoute } from './Routes/privateRoute';
+import PrivateRoute from './Routes/PrivateRoute';
 
 const App = () => (
   <BrowserRouter>

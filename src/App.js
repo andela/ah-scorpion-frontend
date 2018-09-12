6b093606c0988b65d8954a988f0b0ffa936b634c
Ruth Waiganjo <ruthnwaiganjo@gmail.com>
2018-09-12 11:42:54 +0300

    [ft #160411215] Dante skeleton

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ import ConfirmPasswordForm from './containers/ConfirmPasswordForm';
 import SignUp from './containers/SignUp';
 import Dashboard from './containers/Dashboard';
 import PrivateRoute from './routes/PrivateRoute';
+import TextArea from './containers/TextArea';
 
 const App = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact render={props => <Homepage {...props} />} />
-      <Route path="/login" exact render={props => <Login {...props} />} />
-      <Route path="/signup" exact render={props => <SignUp {...props} />} />
-      <Route path="/reset" exact render={props => <ResetForm {...props} />} />
+      <Route path="/" exact render={(props) => <Homepage {...props} />} />
+      <Route path="/login" exact render={(props) => <Login {...props} />} />
+      <Route path="/signup" exact render={(props) => <SignUp {...props} />} />
+      <Route path="/reset" exact render={(props) => <ResetForm {...props} />} />
       <Route
         path="/api/v1/confirm-password/:token"
         exact
-        render={props => <ConfirmPasswordForm {...props} />}
+        render={(props) => <ConfirmPasswordForm {...props} />}
       />
       <PrivateRoute path="/dashboard" component={Dashboard} />
+      <Route path="/new-article" exact component={TextArea} />
     </Switch>
   </BrowserRouter>
 );

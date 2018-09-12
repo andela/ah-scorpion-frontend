aca06323f7d60af8d4d7b404baa608c6c5ac9ffe
Ruth Waiganjo <ruthnwaiganjo@gmail.com>
2018-09-12 22:05:57 +0300

    [ft #160411215] add draft js to create a rich text editor

--- src/containers/TextArea.js
+++ src/containers/TextArea.js
@@ -1,15 +1,123 @@
+/* eslint-disable react/no-multi-comp */
 import React, { Component } from 'react';
-import Dante from 'Dante2';
+import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor';
+import createInlineToolbarPlugin, {
+  Separator,
+} from 'draft-js-inline-toolbar-plugin';
+import {
+  ItalicButton,
+  BoldButton,
+  UnderlineButton,
+  CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
+  UnorderedListButton,
+  OrderedListButton,
+  BlockquoteButton,
+  CodeBlockButton,
+} from 'draft-js-buttons';
+import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
+import createImagePlugin from 'draft-js-image-plugin';
 
-class TextArea extends Component {
+class HeadlinesPicker extends Component {
+  componentDidMount() {
+    setTimeout(() => {
+      window.addEventListener('click', this.onWindowClick);
+    });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this.onWindowClick);
+  }
+
+  onWindowClick = () =>
+    // Call `onOverrideContent` again with `undefined`
+    // so the toolbar can show its regular content again.
+    this.props.onOverrideContent(undefined);
+
   render() {
+    const buttons = [HeadlineOneButton, HeadlineTwoButton, HeadlineThreeButton];
     return (
-      <div className="wrap-contact2">
-        <h1>New Article</h1>
-        <Dante />
+      <div>
+        {buttons.map((
+          Button,
+          i, // eslint-disable-next-line
+        ) => (
+          <Button key={i} {...this.props} />
+        ))}
       </div>
     );
   }
 }
 
-export default TextArea;
+class HeadlinesButton extends Component {
+  // When using a click event inside overridden content, mouse down
+  // events needs to be prevented so the focus stays in the editor
+  // and the toolbar remains visible  onMouseDown = (event) => event.preventDefault()
+  onMouseDown = (event) => event.preventDefault();
+
+  onClick = () =>
+    // A button can call `onOverrideContent` to replace the content
+    // of the toolbar. This can be useful for displaying sub
+    // menus or requesting additional information from the user.
+    this.props.onOverrideContent(HeadlinesPicker);
+
+  render() {
+    return (
+      <div onMouseDown={this.onMouseDown}>
+        <button onClick={this.onClick}>H</button>
+      </div>
+    );
+  }
+}
+
+const inlineToolbarPlugin = createInlineToolbarPlugin({
+  structure: [
+    BoldButton,
+    ItalicButton,
+    UnderlineButton,
+    CodeButton,
+    Separator,
+    HeadlinesButton,
+    UnorderedListButton,
+    OrderedListButton,
+    BlockquoteButton,
+    CodeBlockButton,
+  ],
+});
+const { InlineToolbar } = inlineToolbarPlugin;
+const plugins = [inlineToolbarPlugin, imagePlugin];
+const text = 'Create an article';
+const imagePlugin = createImagePlugin();
+export default class CustomInlineToolbarEditor extends Component {
+  state = {
+    editorState: createEditorStateWithText(text),
+  };
+
+  onChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+
+  focus = () => {
+    this.editor.focus();
+  };
+
+  render() {
+    return (
+      <div onClick={this.focus}>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          plugins={plugins}
+          ref={(element) => {
+            this.editor = element;
+          }}
+        />
+        <InlineToolbar />
+      </div>
+    );
+  }
+}

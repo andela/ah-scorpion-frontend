98c67d9888de7c0914477499aae37eedfd9020b3
davenjeru <dave.njeru@andela.com>
2018-09-13 19:57:01 +0300

    [Feature #160411455] refactor the modal
    Signed-off-by: davenjeru <dave.njeru@andela.com>

--- src/components/ConfirmDeleteModal.js
+++ src/components/ConfirmDeleteModal.js
@@ -3,6 +3,95 @@ import { Button } from 'react-bootstrap';
 import PropType from 'prop-types';
 import Loader from './Loader';
 
+const ConfirmDeleteModalHeader = () => (
+  <div className="modal-header">
+    <h5
+      className="modal-title mx-auto"
+      id="confirmDeleteModalTilte"
+    >
+      Delete Article?
+    </h5>
+  </div>
+);
+
+const ConfirmDeleteModalBody = ({ deleteFailure, deleteSuccess, errorMessage }) => (
+  <div className="modal-body">
+    {deleteFailure || deleteSuccess ? (
+      <div
+        className={`alert text-center alert-${deleteSuccess ? 'success' : 'danger'}`}
+      >
+        {deleteFailure ? errorMessage : 'You have successfully deleted this article'}
+      </div>
+    ) : (
+      <p className="text-center">
+        Are you sure you want to delete this Article?
+        <br />
+        This process is
+        {' '}
+        <strong className="text-danger">irreversible</strong>
+        .
+      </p>
+    )}
+  </div>
+);
+
+ConfirmDeleteModalBody.propTypes = {
+  errorMessage: PropType.string.isRequired,
+  deleteFailure: PropType.bool.isRequired,
+  deleteSuccess: PropType.bool.isRequired,
+
+};
+
+
+const ConfirmDeleteModalFooter = ({
+  deleteFailure, deleteSuccess, isDeleting,
+  postRequestCleanUp, cancelDelete, confirmDelete, deletedArticleSlug,
+}) => (
+  <div className="modal-footer">
+    {deleteFailure || deleteSuccess ? (
+      <Button
+        bsStyle="secondary"
+        data-dismiss="modal"
+        disabled={isDeleting}
+        onClick={postRequestCleanUp}
+      >
+        Close
+      </Button>
+    ) : (
+      <Button
+        bsStyle="secondary"
+        data-dismiss="modal"
+        disabled={isDeleting}
+        onClick={cancelDelete}
+      >
+        Cancel
+      </Button>
+    )}
+
+    {deleteFailure || deleteSuccess ? (
+      ''
+    ) : (
+      <Button
+        bsStyle="danger"
+        onClick={() => confirmDelete(deletedArticleSlug)}
+        disabled={isDeleting}
+      >
+        {isDeleting ? 'Deleting...' : 'Delete'}
+      </Button>
+    )}
+  </div>
+);
+
+ConfirmDeleteModalFooter.propTypes = {
+  deleteFailure: PropType.bool.isRequired,
+  deleteSuccess: PropType.bool.isRequired,
+  isDeleting: PropType.bool.isRequired,
+  confirmDelete: PropType.func.isRequired,
+  cancelDelete: PropType.func.isRequired,
+  postRequestCleanUp: PropType.func.isRequired,
+  deletedArticleSlug: PropType.string.isRequired,
+};
+
 const ConfirmDeleteModal = (props) => {
   const {
     confirmDelete,
@@ -26,14 +115,7 @@ const ConfirmDeleteModal = (props) => {
     >
       <div className="modal-dialog" role="document">
         <div className="modal-content">
-          <div className="modal-header">
-            <h5
-              className="modal-title mx-auto"
-              id="confirmDeleteModalTilte"
-            >
-              Delete Article?
-            </h5>
-          </div>
+          <ConfirmDeleteModalHeader />
           {isDeleting ? (
             <Loader
               style={{
@@ -42,58 +124,21 @@ const ConfirmDeleteModal = (props) => {
               }}
             />
           ) : (
-            <div className="modal-body">
-              {deleteFailure || deleteSuccess ? (
-                <div
-                  className={`alert text-center alert-${deleteSuccess ? 'success' : 'danger'}`}
-                >
-                  {deleteFailure ? errorMessage : 'You have successfully deleted this article'}
-                </div>
-              ) : (
-                <p className="text-center">
-                Are you sure you want to delete this Article?
-                  <br />
-                This process is
-                  {' '}
-                  <strong className="text-danger">irreversible</strong>
-.
-                </p>
-              )}
-            </div>
+            <ConfirmDeleteModalBody
+              deleteFailure={deleteFailure}
+              deleteSuccess={deleteSuccess}
+              errorMessage={errorMessage}
+            />
           )}
-          <div className="modal-footer">
-            {deleteFailure || deleteSuccess ? (
-              <Button
-                bsStyle="secondary"
-                data-dismiss="modal"
-                disabled={isDeleting}
-                onClick={postRequestCleanUp}
-              >
-                Close
-              </Button>
-            ) : (
-              <Button
-                bsStyle="secondary"
-                data-dismiss="modal"
-                disabled={isDeleting}
-                onClick={cancelDelete}
-              >
-                Cancel
-              </Button>
-            )}
-
-            {deleteFailure || deleteSuccess ? (
-              ''
-            ) : (
-              <Button
-                bsStyle="danger"
-                onClick={() => confirmDelete(deletedArticleSlug)}
-                disabled={isDeleting}
-              >
-                {isDeleting ? 'Deleting...' : 'Delete'}
-              </Button>
-            )}
-          </div>
+          <ConfirmDeleteModalFooter
+            cancelDelete={cancelDelete}
+            confirmDelete={confirmDelete}
+            postRequestCleanUp={postRequestCleanUp}
+            deletedArticleSlug={deletedArticleSlug}
+            isDeleting={isDeleting}
+            deleteFailure={deleteFailure}
+            deleteSuccess={deleteSuccess}
+          />
         </div>
       </div>
     </div>
@@ -101,14 +146,14 @@ const ConfirmDeleteModal = (props) => {
 };
 
 ConfirmDeleteModal.propTypes = {
-  confirmDelete: PropType.func.isRequired,
-  cancelDelete: PropType.func.isRequired,
-  postRequestCleanUp: PropType.func.isRequired,
-  deletedArticleSlug: PropType.string.isRequired,
-  errorMessage: PropType.string.isRequired,
-  isDeleting: PropType.bool.isRequired,
-  deleteFailure: PropType.bool.isRequired,
-  deleteSuccess: PropType.bool.isRequired,
+  confirmDelete: ConfirmDeleteModalFooter.propTypes.confirmDelete.isRequired,
+  cancelDelete: ConfirmDeleteModalFooter.propTypes.cancelDelete.isRequired,
+  postRequestCleanUp: ConfirmDeleteModalFooter.propTypes.postRequestCleanUp.isRequired,
+  deletedArticleSlug: ConfirmDeleteModalFooter.propTypes.deletedArticleSlug.isRequired,
+  errorMessage: ConfirmDeleteModalBody.propTypes.errorMessage.isRequired,
+  isDeleting: ConfirmDeleteModalFooter.propTypes.isDeleting.isRequired,
+  deleteFailure: ConfirmDeleteModalBody.propTypes.deleteFailure.isRequired,
+  deleteSuccess: ConfirmDeleteModalBody.propTypes.deleteSuccess.isRequired,
 };
 
 export default ConfirmDeleteModal;

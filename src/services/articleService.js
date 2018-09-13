84dc8e4ce43b37357a166c4913791a776112626f
davenjeru <dave.njeru@andela.com>
2018-09-13 20:02:30 +0300

    [Feature #160411455] fix jsDocs
    Signed-off-by: davenjeru <dave.njeru@andela.com>

--- src/services/articleService.js
+++ src/services/articleService.js
@@ -7,8 +7,9 @@ const articlesUrl = `${REACT_APP_API_URL}/api/v1/articles/`;
 
 /** Class that represents API calls related to a user's articles */
 class articleService {
-  /** * When getMyArticles is called, it returns a Promise that resolves with the list of articles
-   * or rejects with an error message defined below */
+  /** * This is the API call for getting a user's articles
+   * @returns a Promise that resolves with the list of articles or rejects with an
+   * error message defined below */
   static getMyArticles() {
     const errorMessage = 'We could not get your articles at the moment. '
       + 'If the problem persists, please refresh the page or login again';
@@ -31,9 +32,10 @@ class articleService {
     );
   }
 
-  /** * When deleteMyArticle is called, it returns a Promise that resolves with the list of articles
-   * or rejects with an error message defined below
-   * @param {string} slug - The slug of the article that should be deleted */
+  /** * This is the API call for deleting a user's article
+   * @param {string} slug - The slug of the article that should be deleted
+   * @returns returns a Promise that resolves with the list of articles
+   * or rejects with an error message defined below */
   static deleteMyArticle(slug) {
     const errorMessage = 'An error occurred while deleting your article. '
       + 'Please refresh the page or login again.';

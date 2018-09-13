a4ed6e3b5fc90878159979da8e929fa220a5ef4e
davenjeru <dave.njeru@andela.com>
2018-09-13 20:20:27 +0300

    [Feature #160411455] fix hound violations in jsDocs
    Signed-off-by: davenjeru <dave.njeru@andela.com>

--- src/services/articleService.js
+++ src/services/articleService.js
@@ -8,8 +8,8 @@ const articlesUrl = `${REACT_APP_API_URL}/api/v1/articles/`;
 /** Class that represents API calls related to a user's articles */
 class articleService {
   /** * This is the API call for getting a user's articles
-   * @returns a Promise that resolves with the list of articles or rejects with an
-   * error message defined below */
+   * @returns a Promise that resolves
+   * @return {Promise} resolves with the list of articles */
   static getMyArticles() {
     const errorMessage = 'We could not get your articles at the moment. '
       + 'If the problem persists, please refresh the page or login again';
@@ -34,8 +34,8 @@ class articleService {
 
   /** * This is the API call for deleting a user's article
    * @param {string} slug - The slug of the article that should be deleted
-   * @returns returns a Promise that resolves with the list of articles
-   * or rejects with an error message defined below */
+   * @returns returns a Promise
+   * @return {Promise} that resolves after successful deletion */
   static deleteMyArticle(slug) {
     const errorMessage = 'An error occurred while deleting your article. '
       + 'Please refresh the page or login again.';

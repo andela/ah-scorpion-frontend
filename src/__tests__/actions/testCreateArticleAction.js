ee77fe323f2f7a38d8b4a64ac97779d8ff77a2df
Ruth Waiganjo <ruthnwaiganjo@gmail.com>
2018-09-17 22:47:28 +0300

    [ft #160411215] complete passing tests for create article action

--- src/__tests__/actions/testCreateArticleAction.js
+++ src/__tests__/actions/testCreateArticleAction.js
@@ -2,30 +2,39 @@ import moxios from 'moxios';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import * as types from '../../actions/types';
-import createArticleAction from '../../actions/createArticleAction';
+import {
+  articleSuccess,
+  articleFailure,
+} from '../../actions/createArticleAction';
 
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
 const baseUrl = process.env.REACT_APP_BASE_URL;
 const createUrl = `${baseUrl}/articles/`;
-
-describe('The SignUp actions', () => {
+describe('The createArticle actions', () => {
   beforeEach(() => moxios.install());
   afterEach(() => moxios.uninstall());
 
-  it('should dispatch the signUpSuccess action', async (done) => {
-    moxios.stubRequest(createUrl, data, {
+  it('should dispatch the articleSuccess action', async (done) => {
+    moxios.stubRequest(createUrl, {
       status: 201,
     });
 
-    const returnedAction = [
-      {
-        type: types.ARTICLE_CREATE,
-      },
-    ];
-
+    const returnedAction = [{ type: types.ARTICLE_SUCCESS }];
     const store = mockStore({});
-    await store.dispatch(createArticleAction());
+    await store.dispatch(articleSuccess());
+    expect(store.getActions()).toEqual(returnedAction);
+    done();
+  });
+
+  it('should dispatch the articleFailure action', async (done) => {
+    moxios.stubRequest(createUrl, { status: 400, response: 'error message' });
+
+    const returnedAction = [
+      { type: types.ARTICLE_FAILURE, errors: 'error message' },
+    ];
+    const store = mockStore({});
+    await store.dispatch(articleFailure('error message'));
     expect(store.getActions()).toEqual(returnedAction);
     done();
   });
